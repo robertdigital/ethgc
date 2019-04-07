ccee59f5c03bd7402992944306336b2d72591eac
Hardly Difficult <hardlydiff@gmail.com>
2019-04-07 10:15:08 -0700

    Update webpack.config.base.js (#224)

--- website/build/webpack.config.base.js
+++ website/build/webpack.config.base.js
@@ -76,8 +76,8 @@ module.exports = {
     new VueLoaderPlugin(),
     new CopyWebpackPlugin([
       {
-        from: utils.resolve("static/img"),
-        to: utils.resolve("dist/static/img"),
+        from: utils.resolve("static"),
+        to: utils.resolve("dist/static"),
         toType: "dir"
       }
     ])
