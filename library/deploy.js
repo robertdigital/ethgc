7e800551d33b18f8e005dc8be2c572efbe867474
Hardly Difficult <hardlydiff@gmail.com>
2019-04-08 18:11:10 -0700

    try..catch (#277)

--- library/deploy.js
+++ library/deploy.js
@@ -15,57 +15,61 @@ module.exports.deploy = async (
     "https://goerli.infura.io/v3/1830f67bb051457b8d891301de981bd2"
   ]
 ) => {
-  const hardlyWeb3 = new HardlyWeb3(networkNodes[0]);
-  const ethgc = JSON.parse(
-    fs
-      .readFileSync(`${__dirname}/../contracts/build/contracts/ethgc.json`)
-      .toString()
-  );
-  let json;
-
   try {
-    json = JSON.parse(fs.readFileSync(ETHGC_JSON).toString());
-  } catch (e) {
-    // ignore
-  }
-  if (!json) {
-    json = {};
-  }
-  json.abi = ethgc.abi;
-  json.bytecodeHash = hardlyWeb3.web3.utils.keccak256(ethgc.deployedBytecode);
-  for (let i = 0; i < networkNodes.length; i++) {
-    const networkNode = networkNodes[i];
+    const hardlyWeb3 = new HardlyWeb3(networkNodes[0]);
+    const ethgc = JSON.parse(
+      fs
+        .readFileSync(`${__dirname}/../contracts/build/contracts/ethgc.json`)
+        .toString()
+    );
+    let json;
 
-    const networkWeb3 = new HardlyWeb3(networkNode);
-    if (fromAccount) {
-      networkWeb3.switchAccount(fromAccount);
+    try {
+      json = JSON.parse(fs.readFileSync(ETHGC_JSON).toString());
+    } catch (e) {
+      // ignore
     }
-    const networkId = await networkWeb3.web3.eth.net.getId();
-    let networkBytecodeHash;
-    if (json[networkId]) {
-      const networkBytecode = await networkWeb3.web3.eth.getCode(
-        json[networkId]
-      );
-      networkBytecodeHash = networkWeb3.web3.utils.keccak256(networkBytecode);
+    if (!json) {
+      json = {};
+    }
+    json.abi = ethgc.abi;
+    json.bytecodeHash = hardlyWeb3.web3.utils.keccak256(ethgc.deployedBytecode);
+    for (let i = 0; i < networkNodes.length; i++) {
+      const networkNode = networkNodes[i];
+
+      const networkWeb3 = new HardlyWeb3(networkNode);
+      if (fromAccount) {
+        networkWeb3.switchAccount(fromAccount);
+      }
+      const networkId = await networkWeb3.web3.eth.net.getId();
+      let networkBytecodeHash;
+      if (json[networkId]) {
+        const networkBytecode = await networkWeb3.web3.eth.getCode(
+          json[networkId]
+        );
+        networkBytecodeHash = networkWeb3.web3.utils.keccak256(networkBytecode);
+      }
+
+      if (networkBytecodeHash !== json.bytecodeHash) {
+        // Deploy to this network
+        const contract = new networkWeb3.web3.eth.Contract(json.abi);
+        const tx = await networkWeb3.send(
+          contract.deploy({ data: ethgc.bytecode }), // todo byte or deployed?
+          0,
+          fromAccount ? undefined : privateKey,
+          4200000
+        );
+        const receipt = await networkWeb3.getReceipt(tx);
+        json[networkId] = receipt.contractAddress;
+      }
     }
 
-    if (networkBytecodeHash !== json.bytecodeHash) {
-      // Deploy to this network
-      const contract = new networkWeb3.web3.eth.Contract(json.abi);
-      const tx = await networkWeb3.send(
-        contract.deploy({ data: ethgc.bytecode }), // todo byte or deployed?
-        0,
-        fromAccount ? undefined : privateKey,
-        4200000
-      );
-      const receipt = await networkWeb3.getReceipt(tx);
-      json[networkId] = receipt.contractAddress;
-    }
+    json = JSON.stringify(json, null, 2);
+    await fs.promises.mkdir(`${__dirname}/artifacts/`, { recursive: true });
+    fs.writeFileSync(ETHGC_JSON, json);
+
+    console.log("Contracts deployed");
+  } catch (error) {
+    throw new Error(`Deploy failed ${error}`);
   }
-
-  json = JSON.stringify(json, null, 2);
-  await fs.promises.mkdir(`${__dirname}/artifacts/`, { recursive: true });
-  fs.writeFileSync(ETHGC_JSON, json);
-
-  console.log("Contracts deployed");
 };
