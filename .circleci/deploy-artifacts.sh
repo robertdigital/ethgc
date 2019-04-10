daba0caca82024d593d7d2361a74f841c560d9c1
Hardly Difficult <hardlydiff@gmail.com>
2019-04-10 13:54:52 -0700

    git submodule sync (#321)
    * git submodule sync
    
    * Update artifacts deploy & remove get

--- .circleci/deploy-artifacts.sh
+++ .circleci/deploy-artifacts.sh
@@ -1,39 +1,11 @@
 #!/bin/sh
-# ideas used from https://gist.github.com/motemen/8595451
 
-# Based on https://github.com/eldarlabs/ghpages-deploy-script/blob/master/scripts/deploy-ghpages.sh
-# Used with their MIT license https://github.com/eldarlabs/ghpages-deploy-script/blob/master/LICENSE
-
-# abort the script if there is a non-zero error
 set -e
 
-# show where we are on the machine
-pwd
-remote=$(git config remote.origin.url)
-
-# make a directory to put the gp-pages branch
-mkdir artifacts-branch
-cd artifacts-branch
+cd library/artifacts
 # now lets setup a new repo so we can update the artifacts branch
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
-git init
-git remote add --fetch origin "$remote"
-
-
-# switch into the the artifacts branch
-if git rev-parse --verify origin/artifacts > /dev/null 2>&1
-then
-    git checkout artifacts
-    # delete any old site as we are going to replace it
-    # Note: this explodes if there aren't any, so moving it here for now
-    git rm -rf .
-else
-    git checkout --orphan artifacts
-fi
-
-# copy over or recompile the new site
-cp -a ~/repo/library/artifacts/ethgc.json ./ethgc.json
 
 # stage any changes and new files
 git add -A
@@ -43,7 +15,6 @@ git commit --allow-empty -m "Deploy to artifacts [ci skip]"
 git push --force --quiet origin artifacts
 # go back to where we started and remove the artifacts git repo we made and used
 # for deployment
-cd ..
-rm -rf artifacts-branch
+cd ../..
 
-echo "Finished Deployment!"
+echo "Complete"
