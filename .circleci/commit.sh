9383196cb28ce566231d64ea2b7223180f07eece
Hardly Difficult <hardlydiff@gmail.com>
2019-04-10 13:18:05 -0700

    Lint and commit on CI (#319)
    * Lint and commit on CI
    
    * Fix tabs
    
    * Space

---
+++ .circleci/commit.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# ideas used from https://gist.github.com/motemen/8595451
+
+# Based on https://github.com/eldarlabs/ghpages-deploy-script/blob/master/scripts/deploy-ghpages.sh
+# Used with their MIT license https://github.com/eldarlabs/ghpages-deploy-script/blob/master/LICENSE
+
+# abort the script if there is a non-zero error
+set -e
+
+# show where we are on the machine
+pwd
+remote=$(git config remote.origin.url)
+
+# now lets setup a new repo so we can update the branch
+git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
+git config --global user.name "$GH_NAME" > /dev/null 2>&1
+
+# stage any changes and new files
+git add -A
+# now commit
+git commit --allow-empty -m "Deploy lint and library update"
+# and push, but send any output to /dev/null to hide anything sensitive
+git push --force --quiet
+
+echo "Pushed change"
