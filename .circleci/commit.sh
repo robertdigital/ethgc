21d203a3ac9ab5aff8c0df3dcabe9622fc5257c2
Hardly Difficult <hardlydiff@gmail.com>
2019-04-19 17:42:53 -0700

    Update the auto-lint commit script (#518)
    * Update the auto-lint commit script
    
    * auto-lint and/or artifacts-update

--- .circleci/commit.sh
+++ .circleci/commit.sh
@@ -15,21 +15,15 @@ remote=$(git config remote.origin.url)
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
 
-# stage any changes and new files
-git add -A
-# now commit
-git commit -m "auto-lint"
-
 cd ~/repo/library/artifacts
 git pull --rebase --progress "origin" +refs/heads/artifacts
 cd ~/repo
 
-# stage any changes and new files
-git add -A
-# now commit
-git commit -m "artifacts-update"
-
 if ! git diff-index --quiet HEAD --; then
+  # stage any changes and new files
+  git add -A
+  # now commit
+  git commit -m "auto-lint and/or artifacts-update"
   # and push, but send any output to /dev/null to hide anything sensitive
   git push --force --quiet origin $CIRCLE_BRANCH
 fi
--- website/src/components/TxHistory/index.vue
+++ website/src/components/TxHistory/index.vue
@@ -2,9 +2,19 @@
   <div v-on:tx="onTx">
     Transaction history:
     <ul class="list-group" v-if="transactions.length">
-      <li class="list-group-item" v-for="(tx, index) in transactions" :key="index">
-          <StatusIcon :status="{url: '0x86c74643e51183b739c3f2164455ec6ef5077f9c037d4c657764a77c3470aab1', urlMessage: 'etherscan.com'}" />
-          <!-- <StatusIcon :status="{loadingMessage:'wip<br><small>click to view on EtherScan</small>'}" /> -->
+      <li
+        class="list-group-item"
+        v-for="(tx, index) in transactions"
+        :key="index"
+      >
+        <StatusIcon
+          :status="{
+            url:
+              '0x86c74643e51183b739c3f2164455ec6ef5077f9c037d4c657764a77c3470aab1',
+            urlMessage: 'etherscan.com'
+          }"
+        />
+        <!-- <StatusIcon :status="{loadingMessage:'wip<br><small>click to view on EtherScan</small>'}" /> -->
         Create card
         <Date :date="Date.now()" />
         <span v-tooltip="'Success - 5 confirmations. Mined 20 mins ago'">
@@ -33,14 +43,14 @@ export default {
       transactions: []
     };
   },
-  mounted () {
-    this.transactions = this.$ls.get("transactions") || []
+  mounted() {
+    this.transactions = this.$ls.get("transactions") || [];
     this.$observable.subscribe("tx", this.onTx);
   },
   methods: {
     onTx(tx) {
-      this.transactions.push(tx)
-      this.$ls.set("transactions", this.transactions)
+      this.transactions.push(tx);
+      this.$ls.set("transactions", this.transactions);
     }
   }
 };
