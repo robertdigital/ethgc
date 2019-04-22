01d342b47fdde23eb15cc20427f773b61767801e
Hardly Difficult <hardlydiff@gmail.com>
2019-04-21 18:08:12 -0700

    Update commit.sh (#533)

--- .circleci/commit.sh
+++ .circleci/commit.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
-# ideas used from https://gist.github.com/motemen/8595451
-
 # Based on https://github.com/eldarlabs/ghpages-deploy-script/blob/master/scripts/deploy-ghpages.sh
-# Used with their MIT license https://github.com/eldarlabs/ghpages-deploy-script/blob/master/LICENSE
 
 # abort the script if there is a non-zero error
-# set -e
+set -e
 
-# show where we are on the machine
-pwd
 remote=$(git config remote.origin.url)
 
 # now lets setup a new repo so we can update the branch
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
+
+if ! git diff-index --quiet HEAD --; then
+  # stage any changes and new files
+  git add -A
+  # now commit
+  git commit -m "auto-lint"
+  # and push, but send any output to /dev/null to hide anything sensitive
+  git push --force --quiet origin $CIRCLE_BRANCH > /dev/null 2>&1
+fi
 
 cd ~/repo/library/artifacts
 git pull --rebase --progress "origin" +refs/heads/artifacts
@@ -23,9 +27,7 @@ if ! git diff-index --quiet HEAD --; then
   # stage any changes and new files
   git add -A
   # now commit
-  git commit -m "auto-lint and/or artifacts-update"
+  git commit -m "artifacts-update"
   # and push, but send any output to /dev/null to hide anything sensitive
-  git push --force --quiet origin $CIRCLE_BRANCH
+  git push --force --quiet origin $CIRCLE_BRANCH > /dev/null 2>&1
 fi
-
-echo "Pushed change"
