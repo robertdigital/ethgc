19ca4afed38b2593483cc6a036043ff2b9a00a3d
Hardly Difficult <hardlydiff@gmail.com>
2019-07-27 21:25:51 -0700

    Auto artifacts update (#727)
    * Auto artifacts update
    
    * auto-lint or artifacts update

--- .circleci/commit.sh
+++ .circleci/commit.sh
@@ -10,11 +10,16 @@ remote=$(git config remote.origin.url)
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
 
+# update the submodule
+cd ~/repo/artifacts
+git pull origin artifacts
+cd ~/repo
+
 # stage any changes and new files
 git add -A
 if ! git diff-index --quiet origin/$CIRCLE_BRANCH --; then
   # now commit
-  git commit -m "auto-lint"
+  git commit -m "auto-lint or artifacts update"
   # and push
   git push --set-upstream origin $CIRCLE_BRANCH 
 fi
