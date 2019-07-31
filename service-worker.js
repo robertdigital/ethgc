6ac2f70bbf5f31a487232b9c31603a4b2175e714
hardlydifficult <hardlydiff@gmail.com>
2019-07-31 12:12:55 +0000

    Deploy to GitHub pages [ci skip]

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8819e7b33906f5916b2f8f397ec59b7d.js"
+  "/precache-manifest.0795f156aa5e5e65b36e65d1484cd95b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "ethgc"});
