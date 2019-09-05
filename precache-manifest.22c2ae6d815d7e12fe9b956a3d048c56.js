2ced29a27b3c0f2ae13cea7d79860805eeb94ec5
hardlydifficult <hardlydiff@gmail.com>
2019-09-05 14:34:49 +0000

    Deploy to GitHub pages [ci skip]

---
+++ precache-manifest.22c2ae6d815d7e12fe9b956a3d048c56.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8e8db1bb7c902e09797",
+    "url": "/js/chunk-2d22d3f5.f764c133.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "accda67bd31b55c6f72d",
+    "url": "/css/chunk-5a3ddab8.1e7edb93.css"
+  },
+  {
+    "revision": "6516d8f1827b3ccf66a3",
+    "url": "/css/chunk-2618e298.f1378d79.css"
+  },
+  {
+    "revision": "8ca10c01f259410bbf35",
+    "url": "/js/chunk-2d0c0895.75d5c869.js"
+  },
+  {
+    "revision": "9085702e0b5ef688a8b9",
+    "url": "/js/chunk-2d21ef2c.2afc681d.js"
+  },
+  {
+    "revision": "01eaa07a82c16e18b719",
+    "url": "/js/app.c2172b3f.js"
+  },
+  {
+    "revision": "6516d8f1827b3ccf66a3",
+    "url": "/js/chunk-2618e298.fba504a5.js"
+  },
+  {
+    "revision": "accda67bd31b55c6f72d",
+    "url": "/js/chunk-5a3ddab8.9257d4eb.js"
+  },
+  {
+    "revision": "25ee35ea1c56a729ed61",
+    "url": "/js/chunk-vendors.7176f9e1.js"
+  },
+  {
+    "revision": "ed43a9b178caba26ef329db6e5bbae57",
+    "url": "/img/logo.ed43a9b1.png"
+  },
+  {
+    "revision": "561987d26b520d07b4d972b4aed5718b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01eaa07a82c16e18b719",
+    "url": "/css/app.cc4a0117.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.669b328037d0b26cd1b0efbe78d713ab.js"
+  "/precache-manifest.22c2ae6d815d7e12fe9b956a3d048c56.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "ethgc"});
